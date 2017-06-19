c28495d5c3ec78616b77d327dc2351e1b4feb3cc
Tim Farland <twfarland@gmail.com>
2017-06-19 14:16:20 +0400

    video

--- nav.js
+++ nav.js
@@ -1,5 +1,7 @@
 (function () {
 
+// ---------- slide navigation ----------
+
 var body = document.body;
 var left = document.getElementById('left');
 var right = document.getElementById('right');
@@ -7,8 +9,6 @@ var sections = document.getElementsByTagName('section');
 var progress = document.getElementById('progress-bar');
 var last = sections.length - 1;
 var active = -1;
-
-
 
 function activate (idx) {
     if (sections[active]) {
@@ -19,8 +19,11 @@ function activate (idx) {
     var bg = sections[idx].getAttribute('data-bg');
 
     body.className = theme || '';
-    body.style = bg ? 'background-image:url(' + bg + ')' : '';
-    progress.style = 'width:' + Math.floor(idx / last * 100) + '%';
+    body.style.cssText = bg ? 'background-image:url(' + bg + ')' : '';
+    progress.style.cssText = 'width:' + Math.floor(idx / last * 100) + '%';
+
+    left.style.cssText = idx === 0 ? 'display:none;' : '';
+    right.style.cssText = idx === last ? 'display:none;' : '';
 
     sections[idx].className = "active";
     active = idx;
@@ -45,5 +48,29 @@ window.addEventListener('keydown', function (evt) {
 });
 
 activate(0);
+
+// ---------- video navigation ----------
+
+var videoContainer = document.getElementById('video');
+var videoIFrame = document.getElementById('video-iframe');
+var videoOpenBtns = document.getElementsByClassName('video-button');
+var videoCloseBtn = document.getElementById('video-close');
+
+function openVideo (evt) {
+    var src = evt.target.getAttribute('data-youtube');
+    videoContainer.className = 'video-container visible';
+    videoIFrame.src = 'https://www.youtube.com/embed/' + src;
+}
+
+function closeVideo (evt) {
+    videoContainer.className = 'video-container';
+    videoIFrame.src = '';
+}
+
+for (var i = 0; i < videoOpenBtns.length; i++) {
+    videoOpenBtns[i].addEventListener('click', openVideo);
+}
+
+videoCloseBtn.addEventListener('click', closeVideo);
 
 })();
